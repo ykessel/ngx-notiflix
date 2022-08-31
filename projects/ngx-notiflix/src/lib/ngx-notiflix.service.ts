3155490290d3df2e493a450ac4ff8be1631e6c51
yanniel kessel <yanniel@danngos.com>
2022-08-30 22:37:01 -0400

    Add success notificacion

--- projects/ngx-notiflix/src/lib/ngx-notiflix.service.ts
+++ projects/ngx-notiflix/src/lib/ngx-notiflix.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Block, Confirm, Loading, Notify, Report } from 'notiflix';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,7 @@ import { Injectable } from '@angular/core';
 export class NgxNotiflixService {
 
   constructor() { }
+  success = (message: string): void => {
+    Notify.success(message);
+  }
 }
